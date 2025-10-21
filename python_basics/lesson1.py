0b4fc649188089014c1f7241d89703f95a706eac
Cabrales <vcabrales@cps.k12.il.us>
2025-10-21 09:26:02 -0500

    first commit

---
+++ python_basics/lesson1.py
@@ -0,0 +1,28 @@
+#comments in python start with a #
+#comments explain code and are ignored by the interpeter 
+#variables are used to serve values
+Name="Alice" #these are string values
+age=30 # an ineger is a whole value and justed to store whole numbers
+height= 5.5 # this is used as a float variable 
+# floats are used to store variables
+is_student=True # This is a boolean variable
+# Boolean are used to store true or flase values
+#now we will print variables to the consle
+print("Name:", Name)
+print("Age:", age)
+print("Height:", height)
+print("is_student:", is_student)
+#when we make sentances in python
+#we can use the + symbol to join strings together
+#concatenation joins strings using the + operator
+print("Hello my name is " + Name +". I am " + str (age) + " years old" )
+print(" I am " + str(height) + " feet tall ")
+# This is the od or classic way of formatting strings in python
+# There are newer methods like f-stringd snf the format() method
+print(f"my name is {Name}, I am {age} years old and {height} feet tall.")
+date= "2024-06-15"
+weather= "sunny"
+hobby= "painting"
+city="New York"
+mood= "happy"
+print(f"the city I live in is {city}, the date is {date} and the weather is {weather}. My hobby is {hobby}. My mood right now is {mood}.")
